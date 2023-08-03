812b5dca384276e8afa7194ae4688291af969b5d
gisu113 <gisan133555@gmail.com>
2023-08-03 16:27:37 +0900

    バリデーションの追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -7,6 +7,7 @@ class OrderAddress
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :city
     validates :house_number
+    validates :item_id
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :token, presence: true
