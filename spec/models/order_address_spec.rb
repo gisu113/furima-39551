39909c7f256dfa4521dabb2f17f3350bbc949ad6
gisu113 <gisan133555@gmail.com>
2023-08-03 21:58:18 +0900

    idの代入の修正

--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe OrderAddress, type: :model do
         expect(@order_address.errors.full_messages).to include('Postal code is invalid. Include hyphen(-)')
       end
       it 'prefectureを選択していないと購入できないこと' do
-        @order_address.prefecture_id = '東京'
+        @order_address.prefecture_id = '0'
         @order_address.valid?
         expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
       end
