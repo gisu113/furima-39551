e40dcafcac80fa644523748c86e8a27ab7d7abae
gisu113 <gisan133555@gmail.com>
2023-08-03 16:37:17 +0900

    キーに対しバリューを記載

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -14,8 +14,7 @@ class OrderAddress
   end
 
   def save
-    order = Order.create(item_id:, user_id:)
-    Address.create(postal_code:, city:, house_number:, building_name:,
-                   phone_number:, order_id: order.id, prefecture_id:)
+    order = Order.create(item_id: item_id, user_id: user_id)
+    Address.create(postal_code: postal_code, city: city,house_number: house_number,building_name: building_name,phone_number: phone_number,order_id: order.id,prefecture_id: prefecture_id )
   end
 end
